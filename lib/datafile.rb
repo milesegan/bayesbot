f6a35cb100eb31e89c2f00ea884b41a143a3a47b
Miles Egan <milesegan@gmail.com>
2010-10-17 21:10:49 +0700

    update test script and datafile line count

--- lib/datafile.rb
+++ lib/datafile.rb
@@ -1,16 +1,18 @@
 # datafile loading library
 
 class DataFile
-  attr_reader :points
+  attr_reader :count
 
   def initialize(path)
     @file = open(path)
     @sep = /\s*,\s*/
     klass, *@names = @file.readline.strip.split(@sep)
+    @count = 0
   end
 
   def each_sample
     @file.each_line do |f|
+      @count += 1
       klass, *parts = f.strip.split(@sep)
       next unless parts.size == @names.size
       features = @names.zip(parts).collect { |i| i.join("__") }
