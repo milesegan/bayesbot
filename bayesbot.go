a06291ce27716527e01e513b2ebfcabd0c3c0dfa
Miles Egan <miles@plastaq.com>
2014-07-11 10:09:00 +0700

    Read/write channel on go version works.

--- bayesbot.go
+++ bayesbot.go
@@ -4,31 +4,55 @@ import "fmt"
 import "log"
 import "net/http"
 
-type request struct {
+type writeRequest struct {
+	term     string
+	value    float32
+	response chan string
+}
+
+type readRequest struct {
 	term     string
 	response chan string
 }
 
-func core(read chan *request) {
+func core(read chan *readRequest, write chan *writeRequest) {
+	terms := make(map[string]float32)
 	for {
-		if r, ok := <-read; ok {
-			r.response <- "core wrote string: " + r.term
+		select {
+		case r := <-read:
+			if value, ok := terms[r.term]; ok {
+				r.response <- fmt.Sprintf("core read value: %s -> %f", r.term, value)
+			} else {
+				r.response <- fmt.Sprintf("term %s not found", r.term)
+			}
+		case w := <-write:
+			terms[w.term] = w.value
+			w.response <- fmt.Sprintf("core wrote value: %s -> %f", w.term, w.value)
 		}
 	}
 }
 
 func main() {
-	read := make(chan *request, 100)
-	go core(read)
+	read := make(chan *readRequest, 100)
+	write := make(chan *writeRequest, 100)
+	go core(read, write)
 
-	handler := func(w http.ResponseWriter, req *http.Request) {
-		r := &request{"foo", make(chan string)}
+	readHandler := func(w http.ResponseWriter, req *http.Request) {
+		r := &readRequest{"foo", make(chan string)}
 		read <- r
 		resp := <-r.response
 		fmt.Fprintf(w, resp)
 	}
 
-	http.Handle("/", http.HandlerFunc(handler))
+	writeHandler := func(w http.ResponseWriter, req *http.Request) {
+		r := &writeRequest{"foo", 1.0, make(chan string)}
+		write <- r
+		resp := <-r.response
+		fmt.Fprintf(w, resp)
+	}
+
+	http.Handle("/read", http.HandlerFunc(readHandler))
+	http.Handle("/write", http.HandlerFunc(writeHandler))
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("couldn't start server:", err)
